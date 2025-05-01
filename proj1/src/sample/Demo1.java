2da31d2f2eb74e943f0f7200faf15599ecd67775
new <new@172.16.0.222>
2025-05-01 18:27:14 +0530

    task comp

--- proj1/src/sample/Demo1.java
+++ proj1/src/sample/Demo1.java
@@ -1,10 +1,16 @@
 package sample;
 
 public class Demo1 {
-   public static void main(String[] args) {
+   
+	  public void UPI() {
+		 
+	  }
+	public static void main(String[] args) {
 	//Sample triei
 	   System.out.println("Hello ");
     System.out.println("WELCOME USER ");
 
+    
+     
    }
 }
