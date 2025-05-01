94818c84439d44e7022162e4e5823c155963c28a
new <new@172.16.0.222>
2025-05-01 17:40:07 +0530

    comment added

--- proj1/src/sample/Demo1.java
+++ proj1/src/sample/Demo1.java
@@ -2,7 +2,9 @@ package sample;
 
 public class Demo1 {
    public static void main(String[] args) {
-	System.out.println("Hello ");
+	//Sample triei
+	   System.out.println("Hello ");
     System.out.println("WELCOME USER ");
+
    }
 }
